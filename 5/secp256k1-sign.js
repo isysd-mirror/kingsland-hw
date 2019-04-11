9dc20adcae5918ae7afc80c42179375e9e97e9b9
Ira Miller <isysd@isysd.guld>
2019-04-11 00:43:49 +0000

    calculate hash before signing

--- 5/secp256k1-sign.js
+++ 5/secp256k1-sign.js
@@ -1,7 +1,7 @@
 // node -r esm secp256k1-sign.js 7e4670ae70c98d24f3662c172dc510a085578b9ccc717e6c2f4e547edd960a34
 import * as elliptic from 'elliptic'
 import * as CryptoJS from 'cryptojs'
-
+const SHA256 = CryptoJS.default.Crypto.SHA256
 const ec = new elliptic.ec('secp256k1')
 let PRIVKEY
 
@@ -51,11 +51,11 @@ class Transaction {
     this.senderPubKey = senderPubKey
   }
 
-  calcaulteTransactoinHash () {
+  calculateTransactionHash () {
     // Yo! this is not safely reproducable!
     // JSON does not guarantee things like whitespace or ordering.
     let transactionDataJSON = JSON.stringify(this)
-    this.transactionHash = CryptoJS.SHA256(transactionDataJSON).toString()
+    this.transactionHash = SHA256(transactionDataJSON).toString()
   }
 
   sign (privkey) {
@@ -69,8 +69,8 @@ class Transaction {
 
 function testClass () {
   let transaction = new Transaction('c3293572dbe6ebc60de4a20ed0e21446cae66b17', 'f51362b7351ef62253a227a77751ad9b2302f911', 25000, 10, '2018-02-10T17:53:48.972Z', 'Send to Bob', 'c74a8458cd7a7e48f4b7ae6f4ae9f56c5c88c0f03e7c59cb4132b9d9d1600bba1')
+  transaction.calculateTransactionHash()
   transaction.sign(PRIVKEY)
-  console.log(transaction.senderSignature)
   if (transaction.verify()) {
     console.log('valid signature')
     process.exit(0)
